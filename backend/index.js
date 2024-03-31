41a20589a7731764edc65d66ab76398c06950258
Aditya Verma <adityaverma05092001@gmail.com>
2024-03-31 23:08:41 +0530

    Updates: - test

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,9 @@ const app = express();
 
 //Middlewares
 const corsOptions = {
-  origin: [process.env.FRONTEND_URL],
-  credentials: true,
+  origin: [process.env.FRONTEND_URL]
 }
+app.use(cors())
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
